ec7545da157ad6215f0c51f682fad7fd5e6c190e
Darconex <57312818+Darconex@users.noreply.github.com>
2024-08-28 19:13:27 +0200

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+<footer>
+    <p>&copy; 2024 Site de Cybersécurité. Tous droits réservés.</p>
+    <ul>
+        <li><a href="https://facebook.com">Facebook</a></li>
+        <li><a href="https://twitter.com">Twitter</a></li>
+        <li><a href="https://linkedin.com">LinkedIn</a></li>
+    </ul>
+</footer>
+<script src="js/main.js"></script>
+</body>
+</html>
