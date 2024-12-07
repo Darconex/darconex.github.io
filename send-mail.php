d20b27fee3f492f27c496be13a86ccc04889b1ec
Darconex <57312818+Darconex@users.noreply.github.com>
2024-12-07 10:40:13 +0100

    Update send-mail.php

--- send-mail.php
+++ send-mail.php
@@ -7,25 +7,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $message = htmlspecialchars($_POST['message']);
 
     // Adresse email où les messages seront envoyés
-    $to = "votre-adresse-email@example.com"; // Remplacez par votre adresse email
+    $to = "admin@darconex.com";
+
+    // Sujet de l'email
     $subject = "Nouveau message de $nom via le formulaire de contact";
+
+    // En-têtes de l'email
     $headers = "From: $email\r\n";
     $headers .= "Reply-To: $email\r\n";
     $headers .= "Content-Type: text/plain; charset=utf-8\r\n";
 
-    // Composer le message
+    // Composer le corps de l'email
     $emailBody = "Nom: $nom\n";
     $emailBody .= "Email: $email\n";
     $emailBody .= "Sujet: $sujet\n";
     $emailBody .= "Message:\n$message\n";
 
-    $to = "admin@darconex.com";
-    
     // Envoyer l'email
     if (mail($to, $subject, $emailBody, $headers)) {
-        echo "Message envoyé avec succès.";
+        echo "Votre message a été envoyé avec succès.";
     } else {
-        echo "Une erreur est survenue lors de l'envoi du message.";
+        echo "Une erreur est survenue lors de l'envoi du message. Veuillez réessayer.";
     }
 } else {
     echo "Méthode non autorisée.";
